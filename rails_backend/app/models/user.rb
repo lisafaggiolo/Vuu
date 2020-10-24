eaaf9b276b0d44a4602018711eff842c3798a40c
Maria Khandaker <mariakhandaker@gmail.com>
2020-10-24 18:23:24 -0400

    starting to add seeds

--- rails_backend/app/models/user.rb
+++ rails_backend/app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :first_name, presence: true
 end
