e41c1cf75b3fa9682af848cb21a6d01e72fc7599
Maria Khandaker <mariakhandaker@gmail.com>
2020-10-27 13:41:18 -0400

    commit #4 maybe for APIs

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import axios from "axios";
 
+
 const App = (props) => {
   const [cities, setCities] = useState([]);
   const [provinces, setProvinces] = useState([]);
@@ -26,55 +27,113 @@ const App = (props) => {
         // setState(prev => ({ ...prev, cities : all[0].data, provinces: all[1].data,  users: all[2].data}));
       }).catch((error) => console.log(error.errno));
 
-      function getCOVIDinfo(total_hospitalizations, active_cases, current_phase) {
-        return axios.get('https://api.covid19tracker.ca/provinces')
+      componentDidMount() {
+        function getCOVIDinfo(total_hospitalizations, active_cases, current_phase) {
+          return axios.get('https://api.covid19tracker.ca/provinces')
+          .then(res => {
+            const province = res.data;
+            this.setState({ province });
+            console.log(province);
+          })
+          .catch(error => {
+            console.log(error);
+          });
+        }
+        
+        function getWeatherAndTimeZone (weather, time_zone) { 
+          return axios.get("https://community-open-weather-map.p.rapidapi.com/weather")
+          .then(res => {
+            const city = res.data;
+            this.setState({ city });
+            console.log(city)
+          })
+          .catch(error => {
+            console.log(error);
+          });
+        }
+        
+        function getAveragePriceOfItems() {
+          return axios.get("http://www.numbeo.com:8008/api/city_prices?api_key=your_api_key&query=${city},%20Canada",
+                  axios.get("/api/city_prices_raw?api_key=your_api_key&query=Belgrade, Serbia")
+                  .then(res => {
+                    const city = res.data;
+                    this.setState({ city });
+                    console.log(city)
+                  })
+                  .catch(error => {
+                    console.log(error);
+                  }); 
+        }
+        
+        function getHealthSystemQuality() {
+          return axios.get("/api/city_healthcare?api_key=your_api_key&query=Belgrade, Serbia")
+          .then(res => {
+            const city = res.data;
+            this.setState({ city});
+            console.log(city)
+          })
+          .catch(error => {
+            console.log(error);
+          });
+        }
+        
+        function getQualityOfLifeIndex() {
+          return axios.get("https://community-open-weather-map.p.rapidapi.com/weather")
+          .then(res => {
+            const city = res.data;
+            this.setState({ city });
+            console.log(city)
+          })
+          .catch(error => {
+            console.log(error);
+          });
+        }
+        
+        function getVisibleMinorityMakeup() { 
+          return axios.get("https://community-open-weather-map.p.rapidapi.com/weather")
+          .then(res => {
+            const city = res.data;
+            this.setState({ city });
+            console.log(city)
+          })
+          .catch(error => {
+            console.log(error);
+          });
+        }
+        
+        function getShelterCostToIncomeRatio() {
+          return axios.get("https://community-open-weather-map.p.rapidapi.com/weather")
+          .then(res => {
+            const city = res.data;
+            this.setState({ city });
+            console.log(city)
+          })
+          .catch(error => {
+            console.log(error);
+          });                
+        }
+        
+        function getAverageShelterCosts() {
+          return axios.get("https://community-open-weather-map.p.rapidapi.com/weather")
+        }
+        
+        function getCrimeIndex() {
+          return axios.get("/api/city_crime?api_key=your_api_key&query=Belgrade, Serbia")
+        }
+        
+        function getDistributionofPopulationByAgeGroup() {
+          return axios.get("https://community-open-weather-map.p.rapidapi.com/weather")
+        }
+        
+        function getAirPollution() {
+          return axios.get("/api/city_pollution?api_key=your_api_key&query=Belgrade, Serbia")
+        }
+        
+        function getCanadaIndices() {
+          return axios.get("/api/country_indices?api_key=your_api_key&country=Kuwait")
+        }
       }
-      
-      function getWeatherAndTimeZone (weather, time_zone) { 
-        return axios.get("https://community-open-weather-map.p.rapidapi.com/weather")
-      }
-      
-      function getAveragePriceOfItems() {
-        return axios.get("http://www.numbeo.com:8008/api/city_prices?api_key=your_api_key&query=${city},%20Canada",
-                axios.get("/api/city_prices_raw?api_key=your_api_key&query=Belgrade, Serbia") 
-      }
-      
-      function getHealthSystemQuality() {
-        return axios.get("/api/city_healthcare?api_key=your_api_key&query=Belgrade, Serbia")
-      }
-      
-      function getQualityOfLifeIndex() {
-        return axios.get("https://community-open-weather-map.p.rapidapi.com/weather")
-      }
-      
-      function getVisibleMinorityMakeup() { 
-        return axios.get("https://community-open-weather-map.p.rapidapi.com/weather")
-      }
-      
-      function getShelterCostToIncomeRatio() {
-        return axios.get("https://community-open-weather-map.p.rapidapi.com/weather")
-      }
-      
-      function getAverageShelterCosts() {
-        return axios.get("https://community-open-weather-map.p.rapidapi.com/weather")
-      }
-      
-      function getCrimeIndex() {
-        return axios.get("/api/city_crime?api_key=your_api_key&query=Belgrade, Serbia")
-      }
-      
-      function getDistributionofPopulationByAgeGroup() {
-        return axios.get("https://community-open-weather-map.p.rapidapi.com/weather")
-      }
-      
-      function getAirPollution() {
-        return axios.get("/api/city_pollution?api_key=your_api_key&query=Belgrade, Serbia")
-      }
-      
-      function getCanadaIndices() {
-        return axios.get("/api/country_indices?api_key=your_api_key&country=Kuwait")
-      }
-      
+    }                         
       Promise.all([getCOVIDinfo(), getAverageShelterCosts(), getCrimeIndex(), getDistributionofPopulationByAgeGroup(), getHealthSystemQuality(),
                    getQualityOfLifeIndex(), getAirPollution(), getCanadaIndices(), getShelterCostToIncomeRatio(), getVisibleMinorityMakeup(), getWeatherAndTimeZone()])
       .then(function (results) {
@@ -89,19 +148,21 @@ const App = (props) => {
         const SCIRatio = results[8];
         const visMin = results[9];
         console.log(results);    
-      })
-      .catch(function (error) {
-        console.log(error, errno)
-      }); 
+      }) 
 }, []);
-  
+            
+render() {
   return (
-    <main>
-      <Header />
-      <Footer />
-      
-    </main>
-  );
-}
+    <ul>
+      { this.state.province.map(province => <li>{province.name}</li>)}
+    </ul>
+     <main>
+     <Header />
+     <Footer />
+     
+   </main>
+  )
+} 
+
 
 export default App;
