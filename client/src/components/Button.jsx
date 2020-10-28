045603618ae97379f10a27d87f22320d2088bc3c
lisa <lisa.faggiolo@live.ca>
2020-10-28 08:38:51 -0600

    Buttons are there but colors are NOT.

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import "components/Button.scss";
+import classNames from "classnames";
 
-const Button = () =>{
+
+
+const Button = (props) =>{
+  
+  const buttonClass = classNames("button", {
+    "button--confirm" : props.confirm,
+    "button--danger" : props.danger
+  });
+
     return (
-     <h1>Hello I am Button</h1>
+     <button 
+        className={buttonClass} 
+        onClick={props.onClick}
+        disabled={props.disabled}>
+        { props.children }
+       </button>
     );
   }
 export default Button;
