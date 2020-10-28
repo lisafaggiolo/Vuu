cbaf2ebc08d60a1d2cc1000e00d5f981134ebc5d
Maria Khandaker <mariakhandaker@gmail.com>
2020-10-27 20:12:20 -0400

    changes after code review + adding some new fns

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -17,47 +17,49 @@ export default function App(props) {
   const [cities, setCities] = useState([]);
   const [provinces, setProvinces] = useState([]);
   const [users, setUsers] = useState([]);
-  
- 
-// useEffect(() => {
-//   Promise.all([getAveragePriceOfItems(), getCrimeIndex(), getHealthSystemQuality(), getAirPollution(), getWeatherAndTimeZone()])
-//     .then(function (results) {
-//       const itemPrices = results[0];
-//       const crime = results[1];
-//       const health = results[2];
-//       const air = results[3];
-//       const weather = results[4];
-//       console.log(results);
-//       setData({itemPrices, crime, health, air, weather})
-//     })
-//     .catch(error => {
-//       console.log(error);
-//     });
-//   }, [])  
-    
-
+  const [data, setData] = useState({});
+   
   function getCOVIDinfo() {
     return axios.get('https://api.covid19tracker.ca/provinces')
   }
-  //need API key 
-  function getWeatherAndTimeZone() {
-    return axios.get("https://community-open-weather-map.p.rapidapi.com/weather")
+
+//not working even with API key 
+  function getWeatherAndTimeZone(city) {
+    return axios({
+      "method":"GET",
+      "url":"https://community-open-weather-map.p.rapidapi.com/weather",
+      "headers":{
+      "content-type":"application/octet-stream",
+      "x-rapidapi-host":"community-open-weather-map.p.rapidapi.com",
+      "x-rapidapi-key":"10b3a40fb7mshd9e5b9d36c389b5p12226cjsn89d4b168f866",
+      "useQueryString":true
+      },"params":{
+      "lat":"0",
+      "lon":"0",
+      "callback":"test",
+      "id":"2172797",
+      "lang":"null",
+      "units":"%22metric%22 or %22imperial%22",
+      "mode":"xml%2C html",
+      "q":`${city}`
+      }
+      })
   }
-  
+ //cors issue 
   function getAveragePriceOfItems(city) {
     return axios.get(`http://www.numbeo.com/api/city_prices_raw?api_key=exkod1xx5sorso&query=${city},Canada`)
   }
-  
-  function getHealthSystemQuality() {
-    return axios.get("http://www.numbeo.com/api/city_healthcare?api_key=exkod1xx5sorso&query=${city}, Canada")
+ //cors issue
+  function getHealthSystemQuality(city) {
+    return axios.get(`http://www.numbeo.com/api/city_healthcare?api_key=exkod1xx5sorso&query=${city}, Canada`)
   }
-  
-  function getCrimeIndex() {
-    return axios.get("http://www.numbeo.com/api/city_crime?api_key=exkod1xx5sorso&query=${city}, Canada")
+ //cors  
+  function getCrimeIndex(city) {
+    return axios.get(`http://www.numbeo.com/api/city_crime?api_key=exkod1xx5sorso&query=${city}, Canada`)
   }
-  
-  function getAirPollution() {
-    return axios.get("http://www.numbeo.com/api/city_pollution?api_key=exkod1xx5sorso&query=${city}, Canada")
+ //cors 
+  function getAirPollution(city) {
+    return axios.get(`http://www.numbeo.com/api/city_pollution?api_key=exkod1xx5sorso&query=${city}, Canada`)
   }
   
   function getCanadaIndices() {
@@ -70,8 +72,14 @@ export default function App(props) {
       axios.get("/api/provinces"),
       axios.get("/api/users"),
       getCOVIDinfo(),
-      getAveragePriceOfItems("Montreal")
+      getAveragePriceOfItems("Montreal"),
+      getWeatherAndTimeZone("Montreal"),
+      getHealthSystemQuality("Montreal"),
+      getCrimeIndex("Montreal"),
+      getAirPollution("Montreal"),
+      getCanadaIndices()
     ]).then((all) => {
+      
       const cities = all[0].data.data;
       const provinces = all[1].data.data;
       const users = all[2].data;
@@ -80,13 +88,19 @@ export default function App(props) {
       setCities(cities)
       setProvinces(provinces)
       setUsers(users)
-      // setState(prev => ({ ...prev, cities : all[0].data, provinces: all[1].data,  users: all[2].data}));
+
+      const COVID = all[3].data;
+      const itemPrices = all[4].data;
+      const weather = all[5].data;
+      const health = all[6].data;
+      const crime = all[7].data;
+      const air = all[8].data;
+      const stats = all[9].data;
+      setData({COVID, itemPrices, crime, health, air, weather, stats}) 
     }).catch((error) => console.log(error));
   }, []
 );
-  // <main>
-  //   <h1>Better add all that view Shenanigans here</h1>
-  // </main>
+
   return (
     <Router>
       <div>
