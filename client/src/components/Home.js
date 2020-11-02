93b4d42fba8c4e532855c128f12ec854ff6846e6
lisa <lisa.faggiolo@live.ca>
2020-11-02 10:39:54 -0600

    merge conflicts resolved

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,4 @@
 import React from 'react'
-<<<<<<< HEAD
 import './Home.scss'
 import { Link } from "react-router-dom";
 
@@ -21,33 +20,6 @@ const Home = () => {
         </Link>
         </div>
     </div>
-=======
-import './App.scss';
-import { Link } from "react-router-dom";
-// import 'js/bin/materialize';
-
-const Home = () => {
-  return (
-    <main>
-      <div className="home-page">
-        <div className="section no-pad-bot">
-          <div className="container">
-            <br/>
-            <br/>
-            <br/>
-            <h1 className="header center">Explore</h1>
-            <div className="row center">
-              <h3 className="header col s6 light"><Link to="/quiz" className="white-text">Personalized Selection</Link></h3>
-              <h3 className="header col s6 light"><Link to="/provinces" className="white-text">Provinces</Link></h3>
-            </div>
-            <br/>
-            <br/>
-            <br/>
-          </div>
-        </div>
-      </div>
-    </main>
->>>>>>> main
   )
 }
 
