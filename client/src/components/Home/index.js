367aedbcff99bbc0f3911249b4e654f72ee25e94
lisa <lisa.faggiolo@live.ca>
2020-11-03 17:03:06 -0600

    more styling added

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -4,16 +4,18 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div className="home"
-      <div className="top-of-mountain">
-
-        <h2 className="title">Welcome to Canada</h2>
+    <div> 
+      <div className="home">
+        <div className="top-of-mountain">
+          <h2 className="title">Welcome to Canada</h2>
         {/* <p className="subtitle">Find your new office view.</p> */}
-      <div className="selectors">
-          <Link to="/questions/1">
+          <div className="selectors">
+            <Link to="/questions/1">
             <p>Get your personalized selection </p>
           </Link>
+          </div>  
         </div>
+      </div>
     </div>
   )
 }
