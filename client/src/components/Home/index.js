ee218c9a131705257423ee264f1c111f2d8e948f
Maria Khandaker <mariakhandaker@gmail.com>
2020-11-03 20:15:27 -0500

    francis helped us!!

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -4,16 +4,17 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div className="home"
+    <div className="home">
       <div className="top-of-mountain">
 
         <h2 className="title">Welcome to Canada</h2>
         {/* <p className="subtitle">Find your new office view.</p> */}
+      </div>
       <div className="selectors">
           <Link to="/questions/1">
             <p>Get your personalized selection </p>
           </Link>
-        </div>
+      </div>
     </div>
   )
 }
